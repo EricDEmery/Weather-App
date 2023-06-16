c3033f9b7e70f0af38119fd40e00480062cced0d
Eric Emery <ericemery099@gmail.com>
2023-06-15 20:58:30 -0400

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 const page = document.getElementById("page")
+const label = document.getElementById("label")
+const button = document.getElementById("button")
+const input = document.getElementById("input")
+const location = document.getElementById("location")
+const kel = document.getElementById("kel")
+const far = document.getElementById("far")
+const cel = document.getElementById("cel")
+const conditions = document.getElementById("conditions")
+
+let state = [
+    {
+
+    }
+]
